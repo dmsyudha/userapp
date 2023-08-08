44d2148e31575807282984dcc26076e490ea9d80
Dhimas Yudha Pratama <dmsyudha@live.com>
2023-08-08 12:02:36 +0700

    chore: remove unused files

--- src/components/atoms/LabelAtom.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const LabelAtom = ({ label }) => {
-    return <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{label}</Text>;
-};
-
-export default LabelAtom;
